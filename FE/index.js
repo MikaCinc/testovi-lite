f45ed4f9562b6a3acd91af393bb25a9b5dfb2c1e
MikaCinc <mihajlo.ls00@outlook.com>
2021-11-23 12:28:09 +0100

    initial commit

---
+++ FE/index.js
@@ -0,0 +1,21 @@
+const mockSpojnica = {
+  id: "s-1",
+  title: "Glavni gradovi",
+  questions: [
+    {
+      id: "1",
+      question: "Srbija",
+      answer: "Beograd",
+    },
+    {
+      id: "2",
+      question: "Španija",
+      answer: "Madrid",
+    },
+    {
+      id: "3",
+      question: "Grčka",
+      answer: "Atina",
+    },
+  ],
+};
